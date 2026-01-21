185b1c37d69ce1a485c4b0280e6184f25a2ce96e
seulhuioh <127864159+seulhuioh@users.noreply.github.com>
2026-01-21 21:54:44 +0900

    fix: 브라우저 상단 탭이름 변경

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
-  title: "당근이지 - 동물의 숲 아이템 거래",
+  title: "거동숲 - 동물의 숲 아이템 거래",
   description: "모여봐요 동물의 숲 아이템을 쉽고 빠르게 거래하세요",
 };
 
